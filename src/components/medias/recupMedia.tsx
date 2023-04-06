df70eb8cc453689eb1818071bb960ded05adfb03
WAM <eraserw@gmail.com>
2023-04-06 12:09:13 +0200

    feat page welcom-user with logos

--- src/components/medias/recupMedia.tsx
+++ src/components/medias/recupMedia.tsx
@@ -44,37 +44,79 @@ export default function RecupMedia(props: { TOKEN: string }) {
     console.log(test)
 
     return (
-        <div className="container fluidnpm">
-            <h1 className="color-warning">MEDIAS</h1>
-            <div className="row">
-                <div className="col-3 position-relative">
-                    <img className="m-0" src="./images/disk.png" alt="vinyle" />
-                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+        <div className="container-fluid">
+            <div className="row justify-content-around custom-line">
+                <div className="card bg-black" style={{ width: 18 + 'rem' }}>
+                    <img
+                        className="card-img-top"
+                        src="./images/disk.png"
+                        alt="vos vinyles"
+                    />
+                    <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
                         7
                         <span className="visually-hidden">unread messages</span>
                     </span>
                 </div>
-                <div className="col-3 position-relative">
-                    <img className="m-0" src="./images/disk.png" alt="vinyle" />
-                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                <div className="card bg-black" style={{ width: 18 + 'rem' }}>
+                    <img
+                        className="card-img-top"
+                        src="./images/K7audio-V2.png"
+                        alt="vos vinyles"
+                    />
+                    <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
                         7
                         <span className="visually-hidden">unread messages</span>
                     </span>
                 </div>
-                <div className="col-3 position-relative">
-                    <img className="m-0" src="./images/disk.png" alt="vinyle" />
-                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                <div className="card bg-black" style={{ width: 18 + 'rem' }}>
+                    <div>
+                        <span className="position-absolute top-0 start-33 translate-middle badge rounded-pill bg-danger">
+                            7
+                            <span className="visually-hidden">
+                                unread messages
+                            </span>
+                        </span>
+                    </div>
+                    <img
+                        className="card-img-top h-55 w-50 m-auto"
+                        src="./images/Blu-ray.png"
+                        alt="vos vinyles"
+                    />
+                </div>
+                <div className="card bg-black" style={{ width: 18 + 'rem' }}>
+                    <img
+                        className="card-img-top h-100 w-75 m-auto"
+                        src="./images/livre.png"
+                        alt="vos vinyles"
+                    />
+                    <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
                         7
                         <span className="visually-hidden">unread messages</span>
                     </span>
                 </div>
-                <div className="col-3 position-relative">
-                    <img className="m-0" src="./images/disk.png" alt="vinyle" />
-                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                <div className="card bg-black" style={{ width: 18 + 'rem' }}>
+                    <img
+                        className="card-img-top"
+                        src="./images/PS5.png"
+                        alt="vos vinyles"
+                    />
+                    <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
                         7
                         <span className="visually-hidden">unread messages</span>
                     </span>
                 </div>
+                <div className="card bg-black" style={{ width: 18 + 'rem' }}>
+                    <img
+                        className="card-img-top"
+                        src="./images/Snes.png"
+                        alt="vos vinyles"
+                    />
+                    <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger">
+                        7
+                        <span className="visually-hidden">unread messages</span>
+                    </span>
+                </div>
+                <h1 className="color-success start-50">MEDIAS</h1>
             </div>
         </div>
     )
